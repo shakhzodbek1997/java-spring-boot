3e7d9fb722d22c924c6f1198d2f7b8090e07604b
Shakhzodbek <yuldoshov.shakhzod@gmail.com>
2023-09-20 19:26:06 +0200

    First java Spring Bean and Launching Java Spring Configuration

--- src/main/java/com/in28minutes/springfreamwork/App01GamingBasicJava.java
+++ src/main/java/com/in28minutes/springfreamwork/App01GamingBasicJava.java
@@ -3,13 +3,13 @@ package com.in28minutes.springfreamwork;
 import com.in28minutes.springfreamwork.game.GameRunner;
 import com.in28minutes.springfreamwork.game.PacManGame;
 
-public class AppGamingBasicJava {
+public class App01GamingBasicJava {
 	public static void main(String[] args) {
 		
 //		var marioGame = new MarioGame();
 //		var superContraGame = new SuperContraGame();
 		var game = new PacManGame();
-		var runTheGame = new GameRunner(game);
-		runTheGame.run();
+		var gameRunner = new GameRunner(game);
+		gameRunner.run();
 	}
 }
